336c2319d90e6b856cc17c5b3898d8ed2ebcdb00
ancom-ape <97116305+jakubwolowiec@users.noreply.github.com>
2022-07-04 18:36:04 +0200

    Establishing connection with back end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observer } from 'rxjs';
+import { Book } from './book';
+import { BookService } from './book.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'LibraryManagerApp';
+
+
+
+export class AppComponent implements OnInit {
+  public books!: Book[];
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+      this.getBooks;
+  }
+ booksObserver: Observer<Book[]>= {
+  next: (response:Book[]) => this.books = response,
+  error: (error: HttpErrorResponse) => alert(error.message),
+  complete: () => console.log("Everything's fine")
+};
+  
+
+  public getBooks(): void{
+    this.bookService.getBooks().subscribe(this.booksObserver)
+  }
 }
---
+++ src/app/book.ts
@@ -0,0 +1,8 @@
+export interface Book {
+    id: number;
+    title: string;
+    author: string;
+    pages: number;
+    yearOfRelease: number;
+    imgUrl: string;
+}
