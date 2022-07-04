6d3891e333be33ea297c276d54c40ab03d8f97d8
ancom-ape <97116305+jakubwolowiec@users.noreply.github.com>
2022-07-05 00:17:23 +0200

    Added modal funcionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,4 +33,24 @@ export class AppComponent implements OnInit {
       complete: () => {}
     });
   }
+
+  public onOpenModal(book: Book|null, mode: string):void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if(mode === 'add'){
+      button.setAttribute('data-target', '#addBookModal');
+    }
+    else if(mode === 'edit'){
+      button.setAttribute('data-target', '#editBookModal');
+    }
+    else if(mode === 'delete'){
+      button.setAttribute('data-target', '#deleteBookModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
 }
