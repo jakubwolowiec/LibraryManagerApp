e2324f6b1ed39b5956de679728a2ec433302ffa7
ancom-ape <97116305+jakubwolowiec@users.noreply.github.com>
2022-07-07 22:13:26 +0200

    formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Observer } from 'rxjs';
 import { Book } from './book';
 import { BookService } from './book.service';
 
@@ -12,15 +11,14 @@ import { BookService } from './book.service';
 })
 export class AppComponent implements OnInit {
   public books!: Book[];
-  public updateBook!: Book|null;
-  public deleteBook!: Book|null;
+  public updateBook!: Book | null;
+  public deleteBook!: Book | null;
 
   constructor(private bookService: BookService) {}
 
   ngOnInit(): void {
     this.getBooks();
   }
- 
 
   public getBooks(): void {
     this.bookService.getBooks().subscribe({
@@ -30,29 +28,26 @@ export class AppComponent implements OnInit {
       },
       error: (error: HttpErrorResponse) => {
         alert(error.message);
-      }
+      },
     });
   }
 
-  public onOpenModal(book: Book|null, mode: string):void {
+  public onOpenModal(book: Book | null, mode: string): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
     button.type = 'button';
     button.style.display = 'none';
     button.setAttribute('data-toggle', 'modal');
 
-    if(mode === 'add'){
+    if (mode === 'add') {
       button.setAttribute('data-target', '#addBookModal');
-    }
-    else if(mode === 'edit'){
+    } else if (mode === 'edit') {
       this.updateBook = book;
       button.setAttribute('data-target', '#editBookModal');
-    }
-    else if(mode === 'delete'){
+    } else if (mode === 'delete') {
       this.deleteBook = book;
       button.setAttribute('data-target', '#deleteBookModal');
-    }
-    else if(mode === 'rate'){
+    } else if (mode === 'rate') {
       this.updateBook = book;
       button.setAttribute('data-target', '#rateBookModal');
     }
@@ -60,83 +55,81 @@ export class AppComponent implements OnInit {
     button.click();
   }
 
-  public onAddBook(addForm: NgForm){
+  public onAddBook(addForm: NgForm) {
     document.getElementById('add-book-form')?.click();
     this.bookService.addBook(addForm.value).subscribe({
-      next: (response: Book) =>{console.log(response);
-      this.getBooks();
-      addForm.resetForm();
+      next: (response: Book) => {
+        console.log(response);
+        this.getBooks();
+        addForm.resetForm();
       },
-    
-      error: (error: HttpErrorResponse) => {alert(error.message)}
-      
-    }
-    )
-  }
- 
-  public onUpdateBook(book:Book){
-    
-    this.bookService.updateBook(book).subscribe({
-      next: (response: Book) =>{console.log(response);
-      this.getBooks();
-      
+
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
       },
-    
-      error: (error: HttpErrorResponse) => {alert(error.message)},
-
-      complete: () =>{
-        document.getElementById('update-book-form')?.click();
-        document.getElementById('rate-book-form')?.click();
-
-      }
-      
-    }
-    )
-  }
-
-  public onDeleteBook(id?: number):void{
-    let title = this.deleteBook?.title;
-    this.bookService.deleteBook(id!).subscribe(
-      {
-        next: () => {alert(`Succesfully deleted ${title}`)},
-        error: (error: HttpErrorResponse) => alert(error.message),
-        complete: () =>{this.getBooks();}
-      }
-    )
-    
-  }
-
-  public ifRead(): void{
-    this.books.forEach(book => {
-      console.log("ifRead works");
-const buttonIfRead = document.getElementById(`button${book.id}`);
-const ratingIfRead = document.getElementById(`rating${book.id}`);
-if(book.read == true){
-  buttonIfRead?.classList.add("read");
-  ratingIfRead?.classList.remove("hidden");
-}
-else{
-  buttonIfRead?.classList.add("notRead");
-}
     });
   }
 
-  public searchBooks(key: String):void{
-  const results: Book[] = [];
-  for(const book of this.books){
-  if(book.title.toLowerCase().indexOf(key.toLowerCase()) !== -1 
-  || book.author.toLowerCase().indexOf(key.toLowerCase()) !== -1
-  || book.yearOfRelease.toString().indexOf(key.toLowerCase()) !== -1
-  ){
-    results.push(book);
-  }
-  
-  }
-  this.books = results;
+  public onUpdateBook(book: Book) {
+    this.bookService.updateBook(book).subscribe({
+      next: (response: Book) => {
+        console.log(response);
+        this.getBooks();
+      },
 
-  if(results.length === 0 || key === ""){
-    this.getBooks();
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      },
+
+      complete: () => {
+        document.getElementById('update-book-form')?.click();
+        document.getElementById('rate-book-form')?.click();
+      },
+    });
   }
-  
+
+  public onDeleteBook(id?: number): void {
+    let title = this.deleteBook?.title;
+    this.bookService.deleteBook(id!).subscribe({
+      next: () => {
+        alert(`Succesfully deleted ${title}`);
+      },
+      error: (error: HttpErrorResponse) => alert(error.message),
+      complete: () => {
+        this.getBooks();
+      },
+    });
+  }
+
+  public ifRead(): void {
+    this.books.forEach((book) => {
+      console.log('ifRead works');
+      const buttonIfRead = document.getElementById(`button${book.id}`);
+      const ratingIfRead = document.getElementById(`rating${book.id}`);
+      if (book.read == true) {
+        buttonIfRead?.classList.add('read');
+        ratingIfRead?.classList.remove('hidden');
+      } else {
+        buttonIfRead?.classList.add('notRead');
+      }
+    });
+  }
+
+  public searchBooks(key: String): void {
+    const results: Book[] = [];
+    for (const book of this.books) {
+      if (
+        book.title.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        book.author.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        book.yearOfRelease.toString().indexOf(key.toLowerCase()) !== -1
+      ) {
+        results.push(book);
+      }
+    }
+    this.books = results;
+
+    if (results.length === 0 || key === '') {
+      this.getBooks();
+    }
   }
 }
